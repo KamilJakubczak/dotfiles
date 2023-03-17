4ef7579b5c32e08052a7dec7d6099076cdcc2b7c
kamiljakubczak <kamiljakubczak@outlook.com>
2023-03-17 23:54:05 +0100

    update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,11 +4,12 @@
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
 Make sure you use the name(s) of the bar(s) from your config.
